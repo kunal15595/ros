0eb335af371f4909837d4de6b84e500a05eadf52
Kunal Khandelwal <kunal15595@gmail.com>
2014-09-09 20:34:21 +0530

    added ref

---
+++ ws_eclipse/.metadata/.plugins/org.eclipse.core.resources/.history/16/001e4bf80238001413c1c96131792ada
@@ -0,0 +1,5 @@
+package hello_world;
+
+public class hello_world {
+
+}
---
+++ ws_eclipse/hello_world/src/hello_world/hello_world.java
@@ -0,0 +1,8 @@
+package hello_world;
+
+public class hello_world {
+
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+	}
+}
